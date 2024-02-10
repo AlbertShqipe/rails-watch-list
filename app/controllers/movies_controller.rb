bcf47a65f614fda81086f13d66f8730bc9a2a86c
Albert Nikolli <albert_nikolli@icloud.com>
2024-02-10 20:59:29 +0100

    Added the movie controller with the actions new and create to give the possibilty to the user to also add a movie to later be able to relate to a list and also a bookmark

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,20 @@
+class MoviesController < ApplicationController
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+      if @movie.save
+        redirect_to movies_path(@movie)
+      else
+        render :new, status: :unprocessable_entity
+      end
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :poster_url, :rating)
+  end
+
+end
